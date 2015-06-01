c636e8887663f8f7196b2559fd68b0e304a1a53c
Jason Cabot <jasoncabot@gmail.com>
2015-06-01 17:28:08 +0100

    Fixed formatting

--- src/test/com/jasoncabot/gardenpath/model/GameTest.java
+++ src/test/com/jasoncabot/gardenpath/model/GameTest.java
@@ -7,7 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GameTest
 {
     @Test
-    public void aNewlyCreatedGameShouldBeInUnknownState() {
+    public void aNewlyCreatedGameShouldBeInUnknownState()
+    {
         assertThat(new Game().getState()).isEqualTo(Game.State.UNKNOWN);
     }
 
