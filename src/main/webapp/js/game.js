355aa863171bfa015feb1d350820ca69eed7561a
Jason Cabot <jasoncabot@gmail.com>
2015-06-12 17:14:14 +0100

    Added some lovely (horrible) Javascript for laying out a basic game

--- src/main/webapp/js/game.js
+++ src/main/webapp/js/game.js
@@ -1,39 +1,57 @@
 "use strict";
 
-var svgNS = "http://www.w3.org/2000/svg";
-
-
-
 function createBoard() {
-    var board = new Board();
-    board.move("player1", 4);
-    board.move("player2", 76);
-
-    console.log(board.pathFrom(4,76));
-    console.log(board.pathFrom(76,4));
-
-
 
     var boardView = document.getElementById("board");
 
-
     for (var y = 0; y < 9; y++) {
         for (var x = 0; x < 9; x++) {
-            var cell = document.createElementNS(svgNS,'rect');
-            cell.setAttribute('x', (x*50) + (x*10));
-            cell.setAttribute('y', (y*50) + (y*10));
-            cell.setAttribute('width', 50);
-            cell.setAttribute('height', 50);
+            var cell = document.createElement('div');
             cell.setAttribute('class', 'cell');
-            cell.addEventListener('click', onCellSelected);
+            cell.setAttribute('id', 'cell'+((y*9)+x));
+            cell.addEventListener("click", moveHandler, false);
             boardView.appendChild(cell);
         }
     }
 
+    for (var y = 0; y < 10; y++) {
+        for (var x = 0; x < 10; x++) {
+            var post = document.createElement('div');
+            post.setAttribute('class', 'post');
+            post.setAttribute('id', 'post'+((y*9)+x));
+            post.style.left = ((x * 64)+(x*10)) + 'px';
+            post.style.top =((y * 64)+(y*10)) + 'px';
+            post.addEventListener("click", postHandler, false);
+            boardView.appendChild(post);
+        }
+    }
 
-
+    [{id:'player1',name:'jason',pos:{x:50,y:90}}, {id:'player2',name:'cabot',pos:{x:87,y:10}}].forEach(function(data) {
+        var player = document.createElement('div');
+        player.setAttribute('class', 'player');
+        player.setAttribute('id', data.id);
+        player.setAttribute('name', data.name);
+        player.style.top = data.pos.y + 'px';
+        player.style.left = data.pos.x + 'px';
+        boardView.appendChild(player);
+    });
 }
 
-function onCellSelected(e) {
-    console.log('selected cell '+e.relatedTarget);
+function moveHandler(e) {
+    console.log("moved player to " + e.target.id);
+}
+
+var prev;
+
+function postHandler(e) {
+    if (prev == null) {
+        prev = e.target;
+        e.target.style.opacity = 1;
+    } else {
+        prev.style.opacity = 0.1;
+        console.log("building fence from " + prev.id + " to " + e.target.id);
+        prev = null;
+        // submit fence move
+
+    }
 }
