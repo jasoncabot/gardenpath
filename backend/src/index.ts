675153369dbd4a3e7e461eec5420913bf765e9f9
Jason Cabot <jasoncabot@gmail.com>
2021-04-05 13:33:17 +0100

    Backend sample now includes 4 players

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,18 +10,34 @@ app.get("/", (req, res) => {
     const game: InProgressGame = {
         id: 1,
         me: {
-            name: "Jason",
-            position: 71,
+            name: "Player 1",
+            position: 76,
+            colour: 0x00C2FB,
             fences: [
             ],
             target: [0, 1, 2, 3, 4, 5, 6, 7, 8]
         },
         opponents: [{
-            name: "Jason",
-            position: 70,
+            name: "Player 2",
+            position: 4,
+            colour: 0x25D3BA,
             fences: [
             ],
             target: [72, 73, 74, 75, 76, 77, 78, 79, 80]
+        }, {
+            name: "Player 3",
+            position: 36,
+            colour: 0xFF8119,
+            fences: [
+            ],
+            target: [8, 17, 26, 35, 44, 53, 62, 71, 80]
+        }, {
+            name: "Player 4",
+            position: 44,
+            colour: 0xAC47C7,
+            fences: [
+            ],
+            target: [0, 9, 18, 27, 36, 45, 54, 63, 72]
         }],
         myTurn: true,
         lastMoveAt: Date.now(),
