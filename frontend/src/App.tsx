5af76b19a580aca0bb4269023d26bd200eb6c953
Jason Cabot <jasoncabot@gmail.com>
2022-07-21 08:30:21 +0100

    Added Tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { greeter } from "@app/shared";
+import { greeter } from '@app/shared';
 
 function App() {
   const message = greeter();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline text-red-600">
+      {message}
+    </h1>
   );
 }
 
